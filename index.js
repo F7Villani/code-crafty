d33b02725a87b196c0c111e27fe4ce22fbebf3cc
Felipe Villani <felipevillani.2000@hotmail.com>
2024-02-22 20:42:46 -0300

    refact: organizando funcoes

--- index.js
+++ index.js
@@ -4,32 +4,62 @@ import $ from "jquery";
 initializePage();
 
 function initializePage(){
-    $("#create").on("click", createQRCode);
+    $("#create").on("click", handleCreateQRCodeClick);
 }
 
-function createQRCode(){
+async function handleCreateQRCodeClick(){
+
+    toogleLoader();
+
+    // await createQRCode
+
+    // Remove loader
+
+    let imageSource = await createQRCode();
+
+    toogleLoader();
+
+    $(".qrcode").attr("src", imageSource);
+    $("#solution").css("display", "flex");
+    $("#download").attr("download", "qrcode.png");
+    $("#download").attr("href", imageSource);
+}
+
+async function createQRCode(){
     
     let source = $("input[type='text']").val();
     let type = getType(source);
 
-    //if(type === 'email'){
-    //    source = `mailto:${source}`;
-    //}
-
     let qrCode = qr.imageSync(source, { type: 'png', margin: 3, size: 12 });
  
-    let image = URL.createObjectURL(
+    let imageSource = URL.createObjectURL(
         new Blob([qrCode.buffer], { type: 'image/png' })
     );
 
-    $(".qrcode").attr("src", image);
-    $("#solution").css("visibility", "visible");
-    $("#download").attr("download", `${type}.png`);
-    $("#download").attr("href", image);''
+    await timeout(2000);
 
+    return imageSource; 
+}
+
+function toogleLoader(){
+
+    let isLoading = $(".loader-container").css("display") !== "none";
+    if(isLoading){
+        $(".loader-container").css("display", "none");
+    }
+    else {
+        $("#solution").css("display", "none");
+        $(".logo").css("display", "none");
+        $(".loader-container").css("display", "flex");
+    }   
 }
 
 function getType(source){
     //TODO: lógica para identificar tipo do texto
     return "email";
 }
+
+function timeout(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
